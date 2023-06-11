c29e632542c1ea13a68010374c649a9c0c76c857
andrew <urgumandrei@gmail.com>
2023-06-11 14:34:43 +0200

    homework #10

--- script.js
+++ script.js
@@ -1,93 +1,43 @@
-// ================== Homework #9 ==================
+// ================== Homework #10 ==================
 
-let users = [
-  {
-    index: 0,
-    isActive: true,
-    balance: "$2,226.60",
-    name: "Eugenia Sawyer",
-    gender: "female",
-    phone: "+1 (840) 583-3207",
-    address: "949 John Street, Rose, Puerto Rico, 1857",
-  },
-  {
-    index: 1,
-    isActive: true,
-    balance: "$2,613.77",
-    name: "Pauline Gallegos",
-    gender: "female",
-    phone: "+1 (985) 593-3328",
-    address: "328 Greenpoint Avenue, Torboy, North Dakota, 6857",
-  },
-  {
-    index: 2,
-    isActive: false,
-    balance: "$3,976.41",
-    name: "Middleton Chaney",
-    gender: "male",
-    phone: "+1 (995) 591-2478",
-    address: "807 Fleet Walk, Brutus, Arkansas, 9783",
-  },
-  {
-    index: 3,
-    isActive: true,
-    balance: "$1,934.58",
-    name: "Burns Poole",
-    gender: "male",
-    phone: "+1 (885) 559-3422",
-    address: "730 Seba Avenue, Osage, Alabama, 6290",
-  },
-  {
-    index: 4,
-    isActive: true,
-    balance: "$3,261.65",
-    name: "Mcfadden Horne",
-    gender: "male",
-    phone: "+1 (942) 565-3988",
-    address: "120 Scholes Street, Kirk, Michigan, 1018",
-  },
-  {
-    index: 5,
-    isActive: false,
-    balance: "$1,790.56",
-    name: "Suzette Lewis",
-    gender: "female",
-    phone: "+1 (837) 586-3283",
-    address: "314 Dunne Place, Bawcomville, Guam, 9053",
-  },
-];
+const beforeSort = document.querySelector(".before");
+const afterSort = document.querySelector(".after");
+const afterDelete = document.querySelector(".after_delete");
 
-function balanceNumber(balance) {
-  let stringBalance = balance;
-  stringBalance = stringBalance.split("");
-  for (let i = 0; i < stringBalance.length; i++) {
-    if (stringBalance[i] === "$" || stringBalance[i] === ",") {
-      delete stringBalance[i];
-    }
+let number = prompt("Enter the length of the array");
+
+while (number === null || number.trim() === "" || isNaN(number)) {
+  number = prompt("Enter the length of the array(ONLY NUMBER)");
+}
+const lengthArr = Number(number);
+
+let arr = [];
+
+for (let i = 0; i < lengthArr; i++) {
+  let elem = prompt(`Enter the arr[${i}] element`);
+  while (elem === null || elem.trim() === "" || isNaN(elem)) {
+    elem = prompt(`Enter the arr[${i}] element`);
   }
-  stringBalance = Number(stringBalance.join(""));
-
-  return stringBalance;
+  arr.push(Number(elem));
 }
 
-const usersNew = users.map((x) => ({
-  ...x,
-  balance: balanceNumber(x.balance),
-}));
+let html = `<p>${arr}</p>`;
+beforeSort.insertAdjacentHTML("afterend", html);
 
-const contactUsers = usersNew
-  .filter((x) => x.balance > 2000)
-  .reduce((acc, x) => {
-    acc.push(x.phone);
-    return acc;
-  }, []);
+for (let i = 0; i < lengthArr; i++) {
+  for (let j = 0; j < lengthArr - 1; j++) {
+    if (arr[i] < arr[j]) {
+      let tmp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = tmp;
+    }
+  }
+}
 
-const sumBalance = usersNew.reduce((acc, x) => {
-  return (acc += x.balance);
-}, 0);
+html = `<p>${arr}</p>`;
+afterSort.insertAdjacentHTML("afterend", html);
 
-alert(
-  `1.Contact users with balance bigger than 2000$\n ${contactUsers}\n\n2.Sum balance users = ${sumBalance}$ ~ ${sumBalance.toFixed(
-    2
-  )}$`
-);
+arr.splice(1, 3);
+
+html = `<p>${arr}</p>`;
+afterDelete.insertAdjacentHTML("afterend", html);
