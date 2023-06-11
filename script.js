b1f340b5f69dff303bafd152a3a7366ca824ea1c
andrew <urgumandrei@gmail.com>
2023-06-11 12:02:31 +0200

    homework #9

--- script.js
+++ script.js
@@ -1,133 +1,93 @@
-// ================== Homework #8 ==================
+// ================== Homework #9 ==================
 
-const arr = [
-  16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54,
-  76, -4, 12, -35, 4, 47,
+let users = [
+  {
+    index: 0,
+    isActive: true,
+    balance: "$2,226.60",
+    name: "Eugenia Sawyer",
+    gender: "female",
+    phone: "+1 (840) 583-3207",
+    address: "949 John Street, Rose, Puerto Rico, 1857",
+  },
+  {
+    index: 1,
+    isActive: true,
+    balance: "$2,613.77",
+    name: "Pauline Gallegos",
+    gender: "female",
+    phone: "+1 (985) 593-3328",
+    address: "328 Greenpoint Avenue, Torboy, North Dakota, 6857",
+  },
+  {
+    index: 2,
+    isActive: false,
+    balance: "$3,976.41",
+    name: "Middleton Chaney",
+    gender: "male",
+    phone: "+1 (995) 591-2478",
+    address: "807 Fleet Walk, Brutus, Arkansas, 9783",
+  },
+  {
+    index: 3,
+    isActive: true,
+    balance: "$1,934.58",
+    name: "Burns Poole",
+    gender: "male",
+    phone: "+1 (885) 559-3422",
+    address: "730 Seba Avenue, Osage, Alabama, 6290",
+  },
+  {
+    index: 4,
+    isActive: true,
+    balance: "$3,261.65",
+    name: "Mcfadden Horne",
+    gender: "male",
+    phone: "+1 (942) 565-3988",
+    address: "120 Scholes Street, Kirk, Michigan, 1018",
+  },
+  {
+    index: 5,
+    isActive: false,
+    balance: "$1,790.56",
+    name: "Suzette Lewis",
+    gender: "female",
+    phone: "+1 (837) 586-3283",
+    address: "314 Dunne Place, Bawcomville, Guam, 9053",
+  },
 ];
 
-//task 1
-
-const sumPositiveElem = arr
-  .filter((x) => x > 0)
-  .reduce((acc, x) => {
-    return (acc += x);
-  }, 0);
-
-const countPositiveElem = arr
-  .filter((x) => x > 0)
-  .reduce((acc, x) => {
-    return (acc += 1);
-  }, 0);
-
-alert(
-  `TASK #1\nSum of positive elements from array = ${sumPositiveElem}\nCount of positive elements = ${countPositiveElem}`
-);
-
-//task 2
-
-const minElem = arr.reduce((acc, x) => {
-  if (x < acc) {
-    return x;
-  } else {
-    return acc;
+function balanceNumber(balance) {
+  let stringBalance = balance;
+  stringBalance = stringBalance.split("");
+  for (let i = 0; i < stringBalance.length; i++) {
+    if (stringBalance[i] === "$" || stringBalance[i] === ",") {
+      delete stringBalance[i];
+    }
   }
+  stringBalance = Number(stringBalance.join(""));
+
+  return stringBalance;
+}
+
+const usersNew = users.map((x) => ({
+  ...x,
+  balance: balanceNumber(x.balance),
+}));
+
+const contactUsers = usersNew
+  .filter((x) => x.balance > 2000)
+  .reduce((acc, x) => {
+    acc.push(x.phone);
+    return acc;
+  }, []);
+
+const sumBalance = usersNew.reduce((acc, x) => {
+  return (acc += x.balance);
 }, 0);
 
-const indexMinElem = arr.findIndex((x) => x === minElem);
-
 alert(
-  `TASK #2\nMin element from array is ${minElem}\nIt's under index ${indexMinElem}`
+  `1.Contact users with balance bigger than 2000$\n ${contactUsers}\n\n2.Sum balance users = ${sumBalance}$ ~ ${sumBalance.toFixed(
+    2
+  )}$`
 );
-
-//task 3
-
-const maxElem = arr.reduce((acc, x) => {
-  if (x > acc) {
-    return x;
-  } else {
-    return acc;
-  }
-}, 0);
-
-const indexMaxElem = arr.findIndex((x) => x === maxElem);
-
-alert(
-  `TASK #3\nMax element from array is ${maxElem}\nIt's under index ${indexMaxElem}`
-);
-
-//task 4
-
-const countNegativeElem = arr
-  .filter((x) => x < 0)
-  .reduce((acc, x) => {
-    return (acc += 1);
-  }, 0);
-
-alert(`TASK #4\nNumber of negetive elements fro array is ${countNegativeElem}`);
-
-// task 5
-
-const countNoPairPositiveElem = arr
-  .filter((x) => x > 0 && x % 2 !== 0)
-  .reduce((acc, x) => {
-    return (acc += 1);
-  }, 0);
-
-alert(
-  `TASK #5\nNumbers of not pair positive elements from array = ${countNoPairPositiveElem}`
-);
-
-// task 6
-
-const countPairPositiveElem = arr
-  .filter((x) => x > 0 && x % 2 === 0)
-  .reduce((acc, x) => {
-    return (acc += 1);
-  });
-
-alert(
-  `TASK #6\nNumbers of pair positive elements from array = ${countPairPositiveElem}`
-);
-
-// task 7
-
-const sumPairElem = arr
-  .filter((x) => x > 0 && x % 2 === 0)
-  .reduce((acc, x) => {
-    return (acc += x);
-  }, 0);
-
-alert(`TASK #7\nSum of positive pair numbers ${sumPairElem}`);
-
-// task 8
-
-const sumNoPairElem = arr
-  .filter((x) => x > 0 && x % 2 !== 0)
-  .reduce((acc, x) => {
-    return (acc += x);
-  }, 0);
-
-alert(`TASK #8\nSum of positive not pair numbers ${sumNoPairElem}`);
-
-// task 9
-
-const productPositiveElem = arr
-  .filter((x) => x > 0)
-  .reduce((acc, x) => {
-    return (acc *= x);
-  }, 1);
-
-alert(`TASK #9\nProduct of positive numbers ${productPositiveElem}`);
-
-// task 10
-
-const newArr = arr.map((x) => {
-  if (x !== maxElem) {
-    x = 0;
-    return x;
-  } else {
-    return x;
-  }
-});
-
-alert(`TASK #10\n${newArr}`);
