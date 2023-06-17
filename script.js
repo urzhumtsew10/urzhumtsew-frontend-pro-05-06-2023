08ea1b34836be2b2ebd9ab125e8da29cdabffeda
andrew <urgumandrei@gmail.com>
2023-06-17 12:14:30 +0200

    homework #13

--- script.js
+++ script.js
@@ -1,18 +1,82 @@
-// ================== Homework #10 ==================
+// ================== Homework #13 ==================
 
-function removeElement(array, item) {
+// task 1
+
+const getAvgElemOfArray = (array) => {
+  const arrayOfNumbers = array.filter((x) => typeof x === "number");
+  const length = arrayOfNumbers.length;
+
+  const sumElements = arrayOfNumbers.reduce((acc, num) => {
+    return (acc += num);
+  }, 0);
+  return sumElements / length;
+};
+
+const arr = [1, "2", 3, 4, "5", 2, {}, [], 10];
+
+const avgNumberOfArray = getAvgElemOfArray(arr);
+console.log(avgNumberOfArray);
+
+// task 2
+
+const doMath = (x, sign, y) => {
+  switch (sign) {
+    case "+":
+      return x + y;
+    case "-":
+      return x - y;
+    case "*":
+      return x * y;
+    case "/":
+      return x / y;
+    case "^":
+      return x ** y;
+    case "%":
+      return x % y;
+    default:
+      return "We don't know that mathematical sign";
+  }
+};
+
+const x = Number(prompt("Enter first number"));
+const y = Number(prompt("Enter second number"));
+const sign = prompt("Enter mathematical sign");
+const result = doMath(x, sign, y);
+console.log(result);
+
+// task 3
+
+const getDataFromUser = () => {
   let arr = [];
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] !== item) {
-      arr.push(array[i]);
+  const length = prompt("Enter array's length");
+  for (let i = 0; i < length; i++) {
+    arr.push([]);
+    const lengthArr = prompt(`Enter length array under index ${i}`);
+    for (let j = 0; j < lengthArr; j++) {
+      const data = prompt(`Enter data (arr[${i}][${j}])`);
+      arr[i].push(data);
     }
   }
   return arr;
-}
+};
 
-arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(getDataFromUser());
 
-const arrNew = removeElement(arr, 4);
+// task 4
 
-console.log(arr);
-console.log(arrNew);
+const deleteSymbols = (string, arraySymbols) => {
+  let copyString = string.toLowerCase();
+  arraySymbols = arraySymbols.join("").toLowerCase().split("");
+  for (let i = 0; i <= copyString.length; i++) {
+    for (let j = 0; j <= arraySymbols.length; j++) {
+      copyString = copyString.replace(arraySymbols[j], "");
+    }
+  }
+  return copyString;
+};
+
+const string = "I have big garden in New York";
+const symbols = ["A", "G", "I"];
+
+console.log(string);
+console.log(deleteSymbols(string, symbols));
