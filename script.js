7480e1ff0284c95ee357f998ff1757b0bec250c8
andrew <urgumandrei@gmail.com>
2023-06-26 18:16:34 +0200

    homework #19

--- script.js
+++ script.js
@@ -1,102 +1,80 @@
-// ================== Homework #18 ==================
+// ================== Homework #19 ==================
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
-const today = new Date();
-const currentYear = today.getFullYear();
-
-class Student {
-  constructor(name, surname, birthYear) {
-    this.name = name;
-    this.surname = surname;
-    this.birthYear = birthYear;
+class Hamburger {
+  constructor(size, stuffing) {
+    this.size = size;
+    this.stuffing = stuffing;
   }
 
-  attendingLesson = new Array(25);
-  evaluations = [];
+  static SIZE_SMALL = {
+    calories: 20,
+    price: 50,
+  };
+  static SIZE_BIG = {
+    calories: 40,
+    price: 100,
+  };
 
-  getAge() {
-    return currentYear - Number(this.birthYear);
+  static STUFFING_CHEESE = {
+    calories: 20,
+    price: 10,
+  };
+  static STUFFING_SALAD = {
+    calories: 5,
+    price: 20,
+  };
+  static STUFFING_POTATO = {
+    calories: 10,
+    price: 15,
+  };
+
+  static TOPPING_MOYO = {
+    calories: 5,
+    price: 20,
+  };
+  static TOPPING_SAUCE = {
+    calories: 0,
+    price: 15,
+  };
+
+  addTopping(topping) {
+    this.topping = {
+      calories: topping.calories,
+      price: topping.price,
+    };
   }
 
-  present() {
-    if (this.attendingLesson.includes(undefined)) {
-      this.attendingLesson.shift();
-      this.attendingLesson.push(true);
-      this.evaluations.push(getRandomInt(10, 100));
-    } else {
-      console.log("The array already has 25 records");
+  calculate() {
+    if (this.topping) {
+      return (
+        this.size.calories + this.stuffing.calories + this.topping.calories
+      );
     }
+    return this.size.calories + this.stuffing.calories;
   }
 
-  absent() {
-    if (this.attendingLesson.includes(undefined)) {
-      this.attendingLesson.shift();
-      this.attendingLesson.push(false);
-    } else {
-      console.log("The array already has 25 records");
+  calculatePrice() {
+    if (this.topping) {
+      return this.size.price + this.stuffing.price + this.topping.price + "$";
     }
-  }
-
-  getAverageScore() {
-    const sumScores = this.evaluations.reduce((acc, x) => {
-      return (acc += x);
-    }, 0);
-
-    return sumScores / this.evaluations.length;
-  }
-
-  summary() {
-    const visits = this.attendingLesson
-      .filter((x) => x === true)
-      .reduce((acc, x) => {
-        return (acc += 1);
-      }, 0);
-
-    const numberOfLessons = this.attendingLesson.filter(
-      (x) => x === true || x === false
-    ).length;
-
-    if (visits / numberOfLessons >= 0.9 && this.getAverageScore() >= 90) {
-      console.log(`${this.name}, well done!`);
-    } else if (visits / numberOfLessons < 0.9 && this.getAverageScore() < 90) {
-      console.log(`${this.name}, Radish!`);
-    } else if (visits / numberOfLessons < 0.9 || this.getAverageScore() < 90) {
-      console.log(`${this.name}, Good, but can better.`);
-    }
+    return this.size.price + this.stuffing.price + "$";
   }
 }
 
-const student = new Student("Ivan", "Ivanov", 2006);
-console.log(student.getAge());
-student.present();
-student.present();
-student.present();
-student.present();
-student.present();
-student.present();
-student.present();
-student.present();
-student.present();
-student.absent();
-console.log(student.evaluations);
-console.log(student.getAverageScore());
-student.summary();
+const hamburger = new Hamburger(
+  Hamburger.SIZE_SMALL,
+  Hamburger.STUFFING_CHEESE
+);
 
-const student2 = new Student("Petr", "Petrov", 2002);
-console.log(student2.getAge());
-student2.present();
-student2.present();
-student2.present();
-student2.absent();
-student2.present();
-student2.present();
-student2.absent();
-student2.present();
-student2.present();
-student2.absent();
-console.log(student2.evaluations);
-console.log(student2.getAverageScore());
-student2.summary();
+console.log(hamburger.calculate());
+console.log(hamburger.calculatePrice());
+
+hamburger.addTopping(Hamburger.TOPPING_MOYO);
+
+console.log(hamburger.calculate());
+console.log(hamburger.calculatePrice());
+
+hamburger.addTopping(Hamburger.TOPPING_SAUCE);
+
+console.log(hamburger.calculate());
+console.log(hamburger.calculatePrice());
