cb3dfe3ce61acfabe61f8e41a9e926fad9d16ce1
andrew <urgumandrei@gmail.com>
2023-07-10 18:05:06 +0200

    homework 22

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 // ================== Homework #22 ==================
 
 const title = document.querySelector("h1");
-const table = document.createElement("div");
+const table = document.createElement("table");
 title.after(table);
-table.classList.add("table");
 
-for (let i = 1; i <= 100; i++) {
-  table.insertAdjacentHTML("beforeend", `<p>${i}</p>`);
+// table marking
+
+for (let i = 0; i < 10; i++) {
+  const line = document.createElement("tr");
+  table.insertAdjacentElement("beforeend", line);
+  for (let j = 0; j < 10; j++) {
+    const cell = document.createElement("td");
+    line.insertAdjacentElement("beforeend", cell);
+  }
 }
 
-table.style.display = "grid";
-table.style.gridTemplateColumns = "repeat(10, 1fr)";
+const allCell = document.querySelectorAll("td");
+
+for (let i = 0; i < 100; i++) {
+  allCell[i].innerText = i + 1;
+}
