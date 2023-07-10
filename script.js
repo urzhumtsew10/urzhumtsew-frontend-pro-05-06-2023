ff77e7987461e4a6e42f3897491f249f0f22326a
andrew <urgumandrei@gmail.com>
2023-07-10 16:06:48 +0200

    homework #22

--- script.js
+++ script.js
@@ -1,61 +1,13 @@
-// ================== Homework #20 ==================
+// ================== Homework #22 ==================
 
-class SuperMath {
-  input() {
-    const newX = prompt("Enter new X");
-    const newY = prompt("Enter new Y");
-    let newZnak = prompt("Enter new Znak");
-    while (!["+", "-", "*", "/", "%"].includes(newZnak)) {
-      newZnak = prompt("Enter new Znak. + - * / %");
-    }
-    const inputObj = {};
-    inputObj.x = newX;
-    inputObj.y = newY;
-    inputObj.znak = newZnak;
-    return inputObj;
-  }
+const title = document.querySelector("h1");
+const table = document.createElement("div");
+title.after(table);
+table.classList.add("table");
 
-  calculate(obj) {
-    switch (obj.znak) {
-      case "+":
-        return obj.x + obj.y;
-        break;
-      case "-":
-        return obj.x - obj.y;
-        break;
-      case "*":
-        return obj.x * obj.y;
-        break;
-      case "/":
-        return obj.x / obj.y;
-        break;
-      case "%":
-        return obj.x % obj.y;
-        break;
-    }
-  }
+for (let i = 1; i <= 100; i++) {
+  table.insertAdjacentHTML("beforeend", `<p>${i}</p>`);
 }
 
-const obj = {
-  x: 12,
-  y: 2,
-  znak: "-",
-};
-
-SuperMath.prototype.check = function (obj) {
-  const answer = prompt(
-    `Do you want to do action ${obj.znak} with ${obj.x} and ${obj.y}? (YES OR NO)`
-  );
-  if (answer.toLowerCase() === "no") {
-    const newObj = this.input();
-    console.log(this.calculate(newObj));
-  }
-  if (answer.toLowerCase() === "yes") {
-    console.log(this.calculate(obj));
-  }
-};
-
-const p = new SuperMath();
-p.check(obj);
-
-console.log(obj);
+table.style.display = "grid";
+table.style.gridTemplateColumns = "repeat(10, 1fr)";
