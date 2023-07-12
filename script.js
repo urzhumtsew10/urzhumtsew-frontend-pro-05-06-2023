dd6916cbfa521a0e60c140dbb59c484c285ad52f
andrew <urgumandrei@gmail.com>
2023-07-12 18:55:14 +0200

    homework #26

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
-// ================== Homework #25 ==================
+// ================== Homework #26 ==================
 
-let firstLink = prompt("Enter first link");
-if (!firstLink.includes("http") && !firstLink.includes("https")) {
-  firstLink = `https://${firstLink}`;
-}
-let secondLink = prompt("Enter second link");
-if (!secondLink.includes("http") && !secondLink.includes("https")) {
-  secondLink = `https://${secondLink}`;
-}
+const blockVoting = document.querySelector(".voting");
 
-const btnOne = document.querySelector(".btn-1");
-const btnTwo = document.querySelector(".btn-2");
-const blockBtn = document.querySelector("div");
+function generateEmoji(obj) {
+  const keys = Object.keys(obj);
+  const values = Object.values(obj);
 
-blockBtn.addEventListener("click", (e) => {
-  if (e.target.innerText === "First link") {
-    window.open(`${firstLink}`);
+  for (let i = 0; i < keys.length; i++) {
+    blockVoting.insertAdjacentHTML(
+      "beforeend",
+      `
+    <div class="emojiBlock">
+        <img
+          id="${keys[i]}"
+          class="emojiBlock__img"
+          src="./images/${values[i]}"
+          alt="smile"
+        />
+        <p id="${keys[i]}" class="emojiBlock__count">0</p>
+      </div>
+    `
+    );
   }
-  if (e.target.innerText === "Second link") {
-    window.open(`${secondLink}`);
-  }
+}
+
+const obj = {
+  smile: "smiling-face.png",
+  cool: "emoji-cool.png",
+  like: "emoji-like.png",
+  dislike: "emoji-dislike.png",
+  monkey: "emoji-monkey.png",
+};
+
+generateEmoji(obj);
+
+const emojiBlock = document.querySelectorAll(".emojiBlock");
+const count = document.querySelectorAll(".emojiBlock__count");
+
+emojiBlock.forEach((elem) => {
+  elem.addEventListener("click", (e) => {
+    if (e.target.nodeName === "IMG") {
+      count.forEach((count) => {
+        if (count.id === e.target.id) {
+          count.innerText = +count.innerText + 1;
+        }
+      });
+    }
+  });
 });
