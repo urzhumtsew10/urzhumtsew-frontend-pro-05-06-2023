dbb789f6add1aa5149479eaaa052d9cb60cff1b7
andrew <urgumandrei@gmail.com>
2023-07-12 12:24:11 +0200

    homework #24

--- script.js
+++ script.js
@@ -1,30 +1,12 @@
-// ================== Homework #23 ==================
+// ================== Homework #24 ==================
 
-function generationRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
+const input = document.querySelector("input");
+const block = document.querySelector("div");
 
-const images = [
-  "./images/1.jpg",
-  "./images/2.jpg",
-  "./images/3.jpg",
-  "./images/4.jpg",
-  "./images/5.jpg",
-  "./images/6.jpg",
-  "./images/7.jpg",
-  "./images/8.jpg",
-  "./images/9.jpg",
-];
+input.onfocus = () => {
+  block.style.display = "block";
+};
 
-const img = document.createElement("img");
-const btn = document.querySelector(".btn");
-
-img.width = 900;
-img.height = 600;
-img.src = images[generationRandomInt(9)];
-
-btn.before(img);
-
-btn.addEventListener("click", () => {
-  img.src = images[generationRandomInt(9)];
-});
+input.onblur = () => {
+  block.style.display = "none";
+};
