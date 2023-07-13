2323263017e35e08cc9a9f6d5cbbaeb1f4f6e0a2
andrew <urgumandrei@gmail.com>
2023-07-13 23:52:09 +0200

    homework #27

--- script.js
+++ script.js
@@ -1,48 +1,129 @@
-// ================== Homework #26 ==================
+// ================== Homework #27 ==================
 
-const blockVoting = document.querySelector(".voting");
+import { productsData } from "./data.js";
 
-function generateEmoji(obj) {
-  const keys = Object.keys(obj);
-  const values = Object.values(obj);
+const blockCategory = document.querySelector(".category");
 
-  for (let i = 0; i < keys.length; i++) {
-    blockVoting.insertAdjacentHTML(
+function createFilter(array) {
+  for (let i = 0; i < array.length; i++) {
+    blockCategory.insertAdjacentHTML(
       "beforeend",
       `
-    <div class="emojiBlock">
-        <img
-          id="${keys[i]}"
-          class="emojiBlock__img"
-          src="./images/${values[i]}"
-          alt="smile"
-        />
-        <p id="${keys[i]}" class="emojiBlock__count">0</p>
-      </div>
-    `
+        <form>
+          <label>
+            <input class="checkbox" value="${array[i]}" type="checkbox" />
+            ${array[i]}
+          </label>
+        </form>
+        `
     );
   }
+  blockCategory.insertAdjacentHTML(
+    "beforeend",
+    `<button class="filter-btn">Apply</button>`
+  );
 }
 
-const obj = {
-  smile: "smiling-face.png",
-  cool: "emoji-cool.png",
-  like: "emoji-like.png",
-  dislike: "emoji-dislike.png",
-  monkey: "emoji-monkey.png",
-};
+const categories = ["Shoes", "Clothes", "Food", "Technique"];
+createFilter(categories);
 
-generateEmoji(obj);
+const blockProducts = document.querySelector(".products");
 
-const emojiBlock = document.querySelectorAll(".emojiBlock");
-const count = document.querySelectorAll(".emojiBlock__count");
-
-blockVoting.addEventListener("click", (e) => {
-  if (e.target.nodeName === "IMG") {
-    count.forEach((count) => {
-      if (count.id === e.target.id) {
-        count.innerText = +count.innerText + 1;
+function generateProducts(array, filters) {
+  blockProducts.innerHTML = "";
+  for (let i = 0; i < array.length; i++) {
+    filters.forEach((categ) => {
+      if (categ === array[i].category) {
+        blockProducts.insertAdjacentHTML(
+          "beforeend",
+          `
+    <div class="card">
+          <div id="${array[i].id}" class="cover"></div>
+          <img class="card__img" src="./images/${array[i].img}" />
+          <div class="params">
+            <h3 class="params__title">${array[i].title}</h3>
+            <p class="params__price">${array[i].price}</p>
+          </div>
+        </div>
+    `
+        );
       }
     });
   }
+}
+
+generateProducts(productsData, categories);
+
+const aboutProduct = document.querySelector(".aboutProduct");
+
+blockProducts.addEventListener("click", (e) => {
+  aboutProduct.classList.add("active-about");
+  productsData.forEach((elem) => {
+    if (elem.id === +e.target.id) {
+      aboutProduct.insertAdjacentHTML(
+        "beforeend",
+        `
+      <span class="close">X</span>
+      <img src="./images/${elem.img}" class="about__img" />
+        <div class="about__block">
+          <h4 class="about__title">${elem.description}</h4>
+          <div class="about__descr">
+            <div class="descr__price">
+              <p class="text">price:</p>
+              <h4 class="price">${elem.price}</h4>
+            </div>
+            <div class="descr__from">
+              <p class="text">country:</p>
+              <h4 class="country">${elem.country}</h4>
+            </div>
+          </div>
+          <button class="buy">Buy</button>
+        </div>
+      `
+      );
+    }
+  });
+});
+
+const popup = document.querySelector(".popup");
+
+aboutProduct.addEventListener("click", (e) => {
+  if (e.target.nodeName === "SPAN") {
+    aboutProduct.classList.remove("active-about");
+    aboutProduct.innerHTML = "";
+  }
+  if (e.target.nodeName === "BUTTON") {
+    document.body.style.overflow = "hidden";
+    popup.style.display = "flex";
+  }
+});
+
+const checkboxes = document.querySelectorAll(".checkbox");
+
+popup.addEventListener("click", (e) => {
+  if (e.target.classList.value === "popup") {
+    document.body.style.overflow = "auto";
+    popup.style.display = "none";
+    aboutProduct.classList.remove("active-about");
+    aboutProduct.innerHTML = "";
+    checkboxes.forEach((elem) => {
+      elem.checked = false;
+    });
+    generateProducts(productsData, categories);
+  }
+});
+
+const openFilter = document.querySelector(".filter-btn");
+
+openFilter.addEventListener("click", () => {
+  let categoryArray = [];
+  checkboxes.forEach((elem) => {
+    if (elem.checked) {
+      categoryArray.push(elem.value);
+    }
+  });
+  if (categoryArray.length === 0) {
+    categoryArray = categories;
+  }
+  generateProducts(productsData, categoryArray);
 });
