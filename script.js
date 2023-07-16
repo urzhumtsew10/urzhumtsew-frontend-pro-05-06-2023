f62c43a282839b8dbea237dddbc8be92eb93084d
andrew <urgumandrei@gmail.com>
2023-07-16 10:30:39 +0200

    homework #28

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-// ================== Homework #29 ==================
+// ================== Homework #28 ==================
 
-function generateList(array) {
-  const ul = document.createElement("ul");
-  array.forEach((elem) => {
-    if (elem.length > 0) {
-      const li = document.createElement("li");
-      ul.insertAdjacentElement("beforeend", li);
-      const nestedList = document.createElement("ul");
-      li.insertAdjacentElement("beforeend", nestedList);
-      for (let i = 0; i < elem.length; i++) {
-        nestedList.insertAdjacentHTML("beforeend", `<li>1.${elem[i]}</li>`);
-      }
-    } else {
-      ul.insertAdjacentHTML("beforeend", `<li>${elem}</li>`);
-    }
-  });
+const sliderLine = document.querySelector(".slider-line");
+const nextBtn = document.querySelector(".right");
+const prevBtn = document.querySelector(".left");
+prevBtn.style.display = "none";
+let offset = 0;
 
-  return ul;
-}
+nextBtn.addEventListener("click", () => {
+  offset += 840;
+  sliderLine.style.right = offset + "px";
+  if (offset >= 3360) {
+    nextBtn.style.display = "none";
+  }
+  if (offset > 0) {
+    prevBtn.style.display = "block";
+  }
+});
 
-const arr = [1, 2, [1, 2, 3], 3];
-
-console.log(generateList(arr));
+prevBtn.addEventListener("click", () => {
+  offset -= 840;
+  sliderLine.style.right = offset + "px";
+  if (offset === 0) {
+    prevBtn.style.display = "none";
+  }
+  if (offset >= 0) {
+    nextBtn.style.display = "block";
+  }
+});
