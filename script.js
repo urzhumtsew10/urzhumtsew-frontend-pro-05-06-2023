91db613965f48fd13afee916b5598f650f4d16ef
andrew <urgumandrei@gmail.com>
2023-07-21 22:55:24 +0200

    homework №31

--- script.js
+++ script.js
@@ -1,70 +1,60 @@
 // ================== Homework #31 ==================
 const registrationBtn = document.querySelector(".registration");
 const blockRegistration = document.querySelector(".blockRegistration");
-const contentRegistration = document.querySelector(".regContent");
+const formElem = document.querySelector(".regContent");
+const checkboxes = document.querySelectorAll(".checkbox");
 
 registrationBtn.addEventListener("click", () => {
   blockRegistration.style.display = "flex";
 });
 
 blockRegistration.addEventListener("click", (e) => {
-  const checkboxesInfo = [];
-  const regInfo = [];
-  if (e.target.nodeName === "BUTTON") {
-    const infoRegistration = document.querySelectorAll(".reg");
-    infoRegistration.forEach((elem) => {
-      if (elem.type === "checkbox" && elem.checked) {
-        checkboxesInfo.push(elem.value);
-      }
-      if (elem.type !== "checkbox" && elem.value !== "") {
-        if (elem.type === "radio" && elem.checked) {
-          regInfo.push(elem.value);
-        }
-        if (elem.type !== "radio") {
-          regInfo.push(elem.value);
-        }
-      }
-    });
-    console.log(regInfo.length, typeof regInfo.length, regInfo);
-    if (regInfo.length === 6) {
-      contentRegistration.style.boxShadow = "0px 4px 16px green";
-      contentRegistration.innerHTML = "";
-      contentRegistration.insertAdjacentHTML(
-        "beforeend",
-        `
-    <table>
-      <tr>
-        <td>Your Full name:<td>
-        <td>${regInfo[0]} ${regInfo[1]}<td>
-      </tr>
-      <tr>
-        <td>Your Birthday:<td>
-        <td>${regInfo[2]}<td>
-      </tr>
-      <tr>
-        <td>Your city:<td>
-        <td>${regInfo[3]}<td>
-      </tr>
-      <tr>
-        <td>Your Gender:<td>
-        <td>${regInfo[4]}<td>
-      </tr>
-      <tr>
-        <td>You khow languages, such as:<td>
-        <td>${checkboxesInfo}<td>
-      </tr>
-      <tr>
-        <td>Youк adress:<td>
-        <td>${regInfo[5]}<td>
-      </tr>
-    </table>
-    `
-      );
-    } else {
-      contentRegistration.style.boxShadow = "0px 4px 16px red";
-    }
-  }
   if (e.target.classList.value === "blockRegistration") {
     blockRegistration.style.display = "none";
   }
+});
+
+formElem.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(formElem);
+  const entries = formData.entries();
+  const data = Object.fromEntries(entries);
+  const dataBoxes = [];
+  checkboxes.forEach((box) => {
+    if (box.checked) {
+      dataBoxes.push(box.value);
+    }
+  });
+  formElem.innerHTML = "";
+  formElem.insertAdjacentHTML(
+    "beforeend",
+    `
+   <table>
+      <tr>
+        <td>Your Full name:<td>
+        <td>${data.name} ${data.surname}<td>
+      </tr>
+      <tr>
+        <td>Your Birthday:<td>
+        <td>${data.birthday}<td>
+      </tr>
+      <tr>
+        <td>Your city:<td>
+        <td>${data.city}<td>
+      </tr>
+      <tr>
+        <td>Your Gender:<td>
+        <td>${data.gender}<td>
+      </tr>
+      <tr>
+        <td>You khow languages, such as:<td>
+        <td>${dataBoxes}<td>
+      </tr>
+      <tr>
+        <td>Youк adress:<td>
+        <td>${data.adress}<td>
+      </tr>
+    </table>
+  `
+  );
 });
