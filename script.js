ed273800bd24a7f29dc6b54ec39a77168e4b6cd1
andrew <urgumandrei@gmail.com>
2023-08-02 14:48:49 +0200

    homework 33

--- script.js
+++ script.js
@@ -10,46 +10,68 @@ const userId = document.querySelector(".userId");
 const descrPost = document.querySelector(".post-body");
 const btnGetComments = document.querySelector(".comment-btn");
 const blockComments = document.querySelector(".post-comments");
-const request = new XMLHttpRequest();
 
-request.onload = () => {
-  if (request.status === 200) {
-    const data = JSON.parse(request.responseText);
-    divPost.style.display = "block";
-    titlePost.innerText = data.title;
-    userId.innerText = `UserId: ${data.userId}`;
-    descrPost.innerText = data.body;
-  } else {
-    ("Something Went Wrong!");
-  }
-};
+const getPost = new Promise((resolve, reject) => {
+  const request = new XMLHttpRequest();
 
-request.open("GET", `https://jsonplaceholder.typicode.com/posts/${getId}`);
-request.send();
+  request.onload = () => {
+    if (request.status === 200) {
+      resolve(JSON.parse(request.responseText));
+    } else {
+      reject("something went wrong");
+    }
+  };
+
+  request.open("GET", `https://jsonplaceholder.typicode.com/posts/${getId}`);
+  request.send();
+});
+
+getPost.then((value) => {
+  divPost.style.display = "block";
+  titlePost.innerText = value.title;
+  userId.innerText = `UserId: ${value.userId}`;
+  descrPost.innerText = value.body;
+});
+
+getPost.catch((err) => {
+  console.log(`Error: ${err}`);
+});
 
 btnGetComments.addEventListener("click", () => {
   blockComments.innerHTML = "";
-  request.onload = () => {
-    if (request.status === 200) {
-      const data = JSON.parse(request.responseText);
-      data.forEach((comment) => {
-        blockComments.insertAdjacentHTML(
-          "beforeend",
-          `<div class="comment">
+
+  const getComments = new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+
+    request.onload = () => {
+      if (request.status === 200) {
+        resolve(JSON.parse(request.responseText));
+      } else {
+        reject("something went wrong");
+      }
+    };
+
+    request.open(
+      "GET",
+      `https://jsonplaceholder.typicode.com/posts/${getId}/comments`
+    );
+    request.send();
+  });
+
+  getComments.then((value) => {
+    value.forEach((comment) => {
+      blockComments.insertAdjacentHTML(
+        "beforeend",
+        `<div class="comment">
         <h2 class="name-comment">${comment.name}</h2>
         <p class="email-comment">${comment.email}</p>
         <p class="body-comment">${comment.body}</p>
       </div>`
-        );
-      });
-    } else {
-      ("Something Went Wrong!");
-    }
-  };
+      );
+    });
+  });
 
-  request.open(
-    "GET",
-    `https://jsonplaceholder.typicode.com/posts/${getId}/comments`
-  );
-  request.send();
+  getComments.catch((err) => {
+    console.log(`Error: ${err}`);
+  });
 });
